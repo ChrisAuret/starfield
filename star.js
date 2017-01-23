325a185311cb59a23318bae7a43ca0588251c96b
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-24 10:25:21 +1100

    fix up streaks

--- star.js
+++ star.js
@@ -30,13 +30,15 @@ function Star() {
     var sx = map(this.x / this.z, 0, 1, 0, windowWidth);
     var sy = map(this.y / this.z, 0, 1, 0, windowHeight);
 
-    var r = map(this.z, 0, windowWidth, 16, 0);
+    var r = map(this.z, 0, windowWidth, 8, 0);
 
     ellipse(sx, sy, r, r);
 
     var px = map(this.x / this.pz, 0, 1, 0, windowWidth);
     var py = map(this.y / this.pz, 0, 1, 0, windowHeight);
 
+    this.pz = this.z;
+    
     stroke(255);
     line(px, py, sx, sy);
   }
